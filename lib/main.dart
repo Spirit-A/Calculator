7de4a1229c10ef7d7ac806c84367fff741d3dd06
Azhar <spiritsmileya@gmail.com>
2023-06-25 22:32:35 +0000

    Calculatrice fini

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,199 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Calculatrice());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Calculatrice extends StatelessWidget {
+  const Calculatrice({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: "Calculator",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.deepPurple,
+        scaffoldBackgroundColor: Colors.grey[250],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SimpleCalculatrice(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SimpleCalculatrice extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _SimpleCalculatriceState createState() => _SimpleCalculatriceState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SimpleCalculatriceState extends State<SimpleCalculatrice> {
+  String equation = "";
+  String result = "0";
+  String expression = "";
+  bool hasError = false;
 
-  void _incrementCounter() {
+  void _updateDisplayText(String text) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (text == "C") {
+        // Réinitialiser le texte d'affichage
+        equation = "";
+        result = "0";
+        expression = "";
+        hasError = false;
+      } else if (text == "⌫") {
+        // Supprimer le dernier caractère
+        if (!hasError && equation.isNotEmpty) {
+          equation = equation.substring(0, equation.length - 1);
+        } else {
+          equation = "";
+          hasError = false;
+        }
+      } else if (text == "=") {
+        // Évaluer l'expression mathématique
+        if (equation.isNotEmpty) {
+          try {
+            expression = equation;
+
+            // Remplacer les symboles personnalisés par les symboles mathématiques standard
+            expression = expression.replaceAll("×", "*");
+            expression = expression.replaceAll("÷", "/");
+            expression = expression.replaceAll("%", "%");
+
+            // Utiliser une bibliothèque ou une logique pour évaluer l'expression
+            Parser p = Parser();
+            Expression exp = p.parse(expression);
+            ContextModel cm = ContextModel();
+            result = exp.evaluate(EvaluationType.REAL, cm).toString();
+
+            hasError = false;
+          } catch (e) {
+            result = "Erreur de syntaxe";
+            hasError = true;
+          }
+        }
+      } else {
+        // Ajouter le texte au niveau de l'équation
+        if (hasError) {
+          equation = "";
+          hasError = false;
+        }
+
+        if (result != "0") {
+          equation = result;
+          result = "0";
+        }
+
+        equation += text;
+      }
     });
+  }
+
+  Widget calculatriceBtn(String textBtn, Color colorBtn, Color colorText) {
+    return GestureDetector(
+      onTap: () {
+        _updateDisplayText(textBtn);
+      },
+      child: Container(
+        margin: const EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          color: colorBtn,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 3),
+              blurRadius: 6,
+            ),
+          ],
+        ),
+        child: Center(
+          child: Text(
+            textBtn,
+            style: TextStyle(
+              color: colorText,
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: const Text(
+          "Calculatrice",
+          style: TextStyle(
+            color: Color(0xFFE57373),
+          ),
         ),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey[800],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 10, 0),
+                  child: Text(
+                    equation,
+                    style: const TextStyle(fontSize: 35),
+                  ),
+                ),
+                if (!hasError)
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 30, 10, 0),
+                    child: Text(
+                      result,
+                      style: const TextStyle(fontSize: 35),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+          const Divider(),
+          Expanded(
+            flex: 6,
+            child: GridView.count(
+              crossAxisCount: 4,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              children: [
+                calculatriceBtn("C", Colors.redAccent, Colors.white),
+                calculatriceBtn("⌫", Colors.blueGrey, Colors.white),
+                calculatriceBtn("%", Colors.blueGrey, Colors.white),
+                calculatriceBtn("÷", Colors.blueGrey, Colors.white),
+                calculatriceBtn("7", Colors.grey[300]!, Colors.black),
+                calculatriceBtn("8", Colors.grey[300]!, Colors.black),
+                calculatriceBtn("9", Colors.grey[300]!, Colors.black),
+                calculatriceBtn("×", Colors.blueGrey, Colors.white),
+                calculatriceBtn("4", Colors.grey[300]!, Colors.black),
+                calculatriceBtn("5", Colors.grey[300]!, Colors.black),
+                calculatriceBtn("6", Colors.grey[300]!, Colors.black),
+                calculatriceBtn("-", Colors.blueGrey, Colors.white),
+                calculatriceBtn("1", Colors.grey[300]!, Colors.black),
+                calculatriceBtn("2", Colors.grey[300]!, Colors.black),
+                calculatriceBtn("3", Colors.grey[300]!, Colors.black),
+                calculatriceBtn("+", Colors.blueGrey, Colors.white),
+                calculatriceBtn("00", Colors.grey[300]!, Colors.black),
+                calculatriceBtn("0", Colors.grey[300]!, Colors.black),
+                calculatriceBtn(".", Colors.grey[300]!, Colors.black),
+                calculatriceBtn("=", Colors.redAccent, Colors.white),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
